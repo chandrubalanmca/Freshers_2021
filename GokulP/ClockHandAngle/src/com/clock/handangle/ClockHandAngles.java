878fb3a09726b38e07cf3223059c5d516718470b
GokulPrakashKalaiselvan <91322854+GokulPrakashKalaiselvan@users.noreply.github.com>
2021-10-08 17:02:40 +0530

    Updated In ClockHandAngles

--- GokulP/ClockHandAngle/src/com/clock/handangle/ClockHandAngles.java
+++ GokulP/ClockHandAngle/src/com/clock/handangle/ClockHandAngles.java
@@ -1,19 +1,12 @@
 package com.clock.handangle;
 
 public class ClockHandAngles {
-	private int hours;
-	private int minutes;
-	private int seconds;
 	private float hourHandAngle;
 	private float minuteHandAngle;
 	private float secondsHandAngle;
 	public float requiredAngle;
 
 	public ClockHandAngles(int hours, int minutes, int seconds) {
-		super();
-		this.hours = hours;
-		this.minutes = minutes;
-		this.seconds = seconds;
 		hourHandAngle=(hours%12+(float)(minutes/60))*30;
 		minuteHandAngle=(minutes*6)+(seconds/10);
 		secondsHandAngle=seconds*6;
